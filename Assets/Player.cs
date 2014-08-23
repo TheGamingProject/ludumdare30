e4c36ce18a6087a6c29ab40130059a019881f198
Nikolas Poklitar <nikolas.poklitar@gmail.com>
2014-08-22 22:32:29 -0500

    dumb movement

---
+++ Assets/Player.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public Vector2 speed = new Vector2(3.0f, 2.0f);
+	public Vector2 yBounds = new Vector2(-.45f, -3.45f); 
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		float xMovement = Input.GetAxis("Horizontal");
+		float yMovement = Input.GetAxis("Vertical");
+		
+		Vector3 nextPosition = new Vector3 (transform.position.x, transform.position.y, transform.position.z);
+
+		if (xMovement != 0) {
+			nextPosition.x += xMovement * speed.x * Time.deltaTime;
+		}
+
+		if (yMovement != 0) {
+			nextPosition.y += yMovement * speed.y * Time.deltaTime;
+
+			// snap position to y bounds
+			if (nextPosition.y < yBounds.y) {
+				nextPosition.y = yBounds.y;
+			}
+			if (nextPosition.y > yBounds.x) {
+				nextPosition.y = yBounds.x;
+			}
+		}
+
+		transform.position = nextPosition;
+	}
+}
