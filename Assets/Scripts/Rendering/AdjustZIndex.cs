8ec65794cfab9785a29cf09642d637cb53e3a54f
Nikolas Poklitar <nikolas.poklitar@gmail.com>
2014-08-25 10:20:33 -0500

    zindex changes

---
+++ Assets/Scripts/Rendering/AdjustZIndex.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AdjustZIndex : MonoBehaviour {
+	public float ratio = .01f;
+	
+	void Update () {
+		float localZIndex = -ratio * transform.localPosition.y;
+		Debug.Log(localZIndex);
+		Vector3 newPos = new Vector3(transform.localPosition.x, transform.localPosition.y, localZIndex); 
+		transform.localPosition = newPos;
+	}
+}
